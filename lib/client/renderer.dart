433bc16799510ea98fa389fc0cc9540b71d469ad
JohanBaskovec <johan@johanbaskovec.fr>
2019-01-12 22:14:09 +0100

    Render images

--- lib/client/renderer.dart
+++ lib/client/renderer.dart
@@ -7,19 +7,29 @@ import 'package:dart_game/common/game_objects/world.dart';
 import 'package:dart_game/common/tile_position.dart';
 import 'package:dart_game/common/world_position.dart';
 
+enum ImageType { player, tree }
+
 class Renderer {
   final CanvasElement _canvas;
   final CanvasRenderingContext2D _ctx;
   double scale = 1;
   CanvasPosition cameraPosition;
   TilePosition lastPlayerPosition;
+  Map<ImageType, ImageElement> images = {};
 
   Renderer(this._canvas)
-      : _ctx = _canvas.getContext('2d') as CanvasRenderingContext2D;
+      : _ctx = _canvas.getContext('2d') as CanvasRenderingContext2D {
+    images[ImageType.tree] = ImageElement();
+    images[ImageType.tree].src = '/tree.png';
+
+    images[ImageType.player] = ImageElement();
+    images[ImageType.player].src = '/man.png';
+  }
 
   void render(World world) {
     _canvas.width = window.innerWidth;
     _canvas.height = window.innerHeight;
+
     _ctx.scale(scale, scale);
     if (cameraPosition != null) {
       _ctx.translate(cameraPosition.x, cameraPosition.y);
@@ -27,18 +37,16 @@ class Renderer {
     _ctx.clearRect(0, 0, _canvas.width, _canvas.height);
     for (var player in world.players) {
       if (player != null) {
-        _ctx.fillStyle = 'green';
-        _ctx.fillRect(player.box.left, player.box.top, player.box.width,
-            player.box.height);
+        _ctx.drawImageScaled(images[ImageType.player], player.box.left,
+            player.box.top, player.box.width, player.box.height);
       }
     }
 
     for (List<SolidGameObject> column in world.solidObjectColumns) {
       for (SolidGameObject object in column) {
         if (object != null) {
-          _ctx.fillStyle = 'black';
-          _ctx.fillRect(object.box.left, object.box.top, object.box.width,
-              object.box.height);
+          _ctx.drawImageScaled(images[ImageType.tree], object.box.left,
+              object.box.top, object.box.width, object.box.height);
         }
       }
     }
@@ -60,7 +68,8 @@ class Renderer {
   }
 
   WorldPosition getWorldPositionFromCanvasPosition(CanvasPosition position) {
-    return WorldPosition((position.x * (1 / scale)) - cameraPosition.x, (position.y * (1 / scale)) - cameraPosition.y);
+    return WorldPosition((position.x * (1 / scale)) - cameraPosition.x,
+        (position.y * (1 / scale)) - cameraPosition.y);
   }
 
   WorldPosition getCursorPositionInWorld(MouseEvent event) {
