a67e14526ee632c93ddfe929da2cf14e52c5d9eb
JohanBaskovec <johan@johanbaskovec.fr>
2019-01-16 02:21:37 +0100

    Revert "Add Entity class that contains id to components"
    This reverts commit 21eaa60e56fbc7f85b9455effc31eceed078715b.

--- lib/common/game_objects/world.dart
+++ lib/common/game_objects/world.dart
@@ -4,7 +4,6 @@ import 'package:dart_game/client/component/rendering_component.dart';
 import 'package:dart_game/client/component/usable_component.dart';
 import 'package:dart_game/common/box.dart';
 import 'package:dart_game/common/constants.dart';
-import 'package:dart_game/common/entity.dart';
 import 'package:dart_game/common/game_objects/entity_type.dart';
 import 'package:dart_game/common/inventory.dart';
 import 'package:dart_game/common/size.dart';
@@ -17,58 +16,44 @@ part 'world.g.dart';
 @JsonSerializable(anyMap: true)
 class World {
   Size _dimension;
-  List<List<Tile>> tilesColumn = [];
+  static final componentListSize = worldSize.x * worldSize.y * 100;
+  List<List<Tile>> tilesColumn = List(componentListSize);
   List<List<int>> solidObjectColumns = [];
-  List<Inventory> publicInventories = [];
-  List<Inventory> privateInventories = [];
-  List<TilePosition> gridPositions = [];
-  List<int> freeGridPositionsIds = [];
-  List<Entity> entities = [];
-  List<int> freeEntityIds = [];
-  List<RenderingComponent> renderingComponents = [];
-  List<int> freeRenderingComponentsIds = [];
-  List<UsableComponent> usableComponents = [];
+  List<Inventory> publicInventories = List(componentListSize);
+  List<Inventory> privateInventories = List(componentListSize);
+  List<TilePosition> gridPositions = List(componentListSize);
+  List<int> entities = List(componentListSize);
+  List<RenderingComponent> renderingComponents = List(componentListSize);
+  List<UsableComponent> usableComponents = List(componentListSize);
+  int lastAddedEntityId = 0;
 
   World();
 
   int addGridAlignedEntity(EntityType image, TilePosition position) {
     final double x = (position.x * tileSize).toDouble();
     final double y = (position.y * tileSize).toDouble();
-    final Entity entity = addEntity(image);
-    renderingComponents[entity.id].box =
+    final int entityId = addEntity(image);
+    renderingComponents[entityId].box =
         Box(x, y, tileSize.toDouble(), tileSize.toDouble());
-    gridPositions[entity.id] = position;
+    gridPositions[entityId] = position;
     solidObjectColumns[position.x][position.y] = entityId;
     return entityId;
   }
 
-  Entity addEntity(EntityType type) {
+  int addEntity(EntityType type) {
     int entityId;
-    Entity entity;
-    final bool addNewId = freeEntityIds.isEmpty;
-    if (addNewId) {
-      entityId = entities.length;
-      entity = Entity(entityId);
-      entities.add(entity);
-    } else {
-      entityId = freeEntityIds.removeLast();
-      entity = Entity(entityId);
-      entities[entityId] = entity;
+    int i = lastAddedEntityId + 1;
+    while (entityId == null && i != lastAddedEntityId) {
+      if (entities[i] == null) {
+        final int entityId = i;
+        renderingComponents[entityId] = RenderingComponent(type, null);
+        entities[entityId] = entityId;
+        lastAddedEntityId = entityId;
+        return entityId;
+      }
+      i++;
     }
-    addRenderingComponent(entity, RenderingComponent(type, null));
-    return entity;
-  }
-  
-  void addRenderingComponent(Entity entity, RenderingComponent renderingComponent) {
-    int id;
-    final bool addNewId = freeRenderingComponentsIds.isEmpty;
-    if (addNewId) {
-      id = renderingComponents.length;
-      renderingComponents.add(renderingComponent);
-    } else {
-      id = freeRenderingComponentsIds.removeLast();
-    }
-    entity.renderingComponentId = id;
+    return -1;
   }
 
   World.fromConstants(Random randomGenerator)
